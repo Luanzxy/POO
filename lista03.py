f9a4a210ca439db969cf862a748f2c363a12cd4b
Luanzxy <luan.ribeiro@escolar.ifrn.edu.br>
2025-04-24 13:25:03 +0000

    jjj

--- lista03.py
+++ lista03.py
@@ -4,3 +4,12 @@ b = int(input("Digite o segundo valor:"))
 prod = a * b
 print(f"PROD = {prod}")
 
+
+# Questão 2
+nota_a = float(input("Nota A do aluno:"))
+nota_b = float(input("Nota B do aluno:"))
+a = nota_a * 3.5
+b = nota_b * 7.5
+soma_nota = (a + b) 
+nota_final = soma_nota / 
+
